d6324471a03455d2222ef10717ac34b573feb2e9
Samuel Levi <samuellevi.job@gmail.com>
2024-11-05 21:03:51 -0300

    adding liquibase

--- src/main/java/com/techchallenge/fastfood/FastfoodApplication.java
+++ src/main/java/com/techchallenge/fastfood/FastfoodApplication.java
@@ -1,7 +1,10 @@
 package com.techchallenge.fastfood;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 public class FastfoodApplication {
